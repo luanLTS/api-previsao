4afd72a846a5085228bb85c2f121aa3c9c4e6aaf
Luan Teixeira <luanlts@outlook.com>
2022-03-09 17:38:26 -0300

    Instalando a biblioteca axios

---
+++ index.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+const key = "abfe510dc62eecff75c7e32beea72aeb";
+const locale = "Itu";
+const url = `http://api.openweathermap.org/data/2.5/forecast?q=${locale}&appid=${key}`;
