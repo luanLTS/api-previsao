9dc499c43b5c5957ef13fa314bbe39c0f54f585b
Luan Teixeira <luanlts@outlook.com>
2022-03-12 12:42:29 -0300

    testando a api de clima com o axios

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import axios from "axios";
+const axios = require("axios");
 const key = "abfe510dc62eecff75c7e32beea72aeb";
 const locale = "Itu";
 const url = `http://api.openweathermap.org/data/2.5/forecast?q=${locale}&appid=${key}`;
+
+axios.get(url).then((result) => {
+    console.log(result);
+});
